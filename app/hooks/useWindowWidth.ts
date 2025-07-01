11afc45ac8f1efb2badc55459aa58a725bc289df
Gifino Thoriq <gifinothoriq@Gifinos-MacBook-Air.local>
2025-07-01 16:11:14 +0700

    chore: fix build

--- app/hooks/useWindowWidth.ts
+++ app/hooks/useWindowWidth.ts
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
 
 export function useWindowWidth() {
-  // const [windowWidth, setWindowWidth] = useState<number>(() => {
-  //   if (typeof window !== "undefined") {
-  //     return window.innerWidth;
-  //   }
-  //   return 0; // Default value during SSR
-  // });
+  const [windowWidth, setWindowWidth] = useState<number>(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
+    }
+    return 0; // Default value during SSR
+  });
 
-  // useEffect(() => {
-  //   const handleResize = () => setWindowWidth(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
 
-  //   // Set on mount
-  //   handleResize();
+    // Set on mount
+    handleResize();
 
-  //   window.addEventListener("resize", handleResize);
-  //   return () => window.removeEventListener("resize", handleResize);
-  // }, []);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-  return 1;
+  return windowWidth;
 }
